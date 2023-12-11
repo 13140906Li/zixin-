6d494f3fb6144523ec8e3d6225ebe855c68cf8cf
li-zixin <1138133490@qq.com>
2023-12-11 21:44:11 +0800

    初始化后端项目

---
+++ src/test/java/com/yupi/springbootinit/mapper/ChartMapperTest.java
@@ -0,0 +1,21 @@
+package com.yupi.springbootinit.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+class ChartMapperTest {
+
+    @Resource
+    private ChartMapper chartMapper;
+    @Test
+    void queryChartData() {
+        String querySql = "select * from chart";
+        List<Map<String,Object> > resultData = chartMapper.queryChartData(querySql);
+        System.out.println(resultData);
+    }
+}
